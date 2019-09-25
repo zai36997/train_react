48fc199ba33c6f41d6acd40d0d7825fd216bec72
Paramee Thongtep <zai36997@gmail.com>
2019-09-25 16:48:23 +0700

    resolve configureStore

--- src/App.js
+++ src/App.js
@@ -14,30 +14,36 @@ import Shop from "./pages/Shop";
 import Register from "./pages/Register";
 import PrivateRoute from "./guard/auth";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./redux/reducers/index";
+// import { createStore } from "redux";
+// import rootReducer from "./redux/reducers/index";
+import { PersistGate } from "redux-persist/integration/react";
+import configureStore from "./redux/configureStore";
+
 import Cart from "./pages/Cart";
 
 library.add(fab, fas);
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
+const { store, persistor } = configureStore();
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <PrivateRoute path="/about" component={About} />
-          <Route path="/shop/:id" component={Shop}></Route>
-          <Route path="/register" component={Register}></Route>
-          <Route path="/cart" component={Cart}></Route>
-          <Route component={Notfound}></Route>
-        </Switch>
-        <Footer />
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <PrivateRoute path="/about" component={About} />
+            <Route path="/shop/:id" component={Shop}></Route>
+            <Route path="/register" component={Register}></Route>
+            <Route path="/cart" component={Cart}></Route>
+            <Route component={Notfound}></Route>
+          </Switch>
+          <Footer />
 
-        {/* <Home/> */}
-      </Router>
+          {/* <Home/> */}
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,13 +3,13 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
-import rootReducer from "./reducers";
+import rootReducer from "./reducers/index";
 
 const persistConfig = {
   key: "root",
   storage,
   stateReconciler: autoMergeLevel2,
-  whitelist: ["certReducer"]
+  whitelist: ["cartReducer"]
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
